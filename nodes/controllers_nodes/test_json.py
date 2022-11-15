58e87c19802d52e3d88aea62272ed89ca0e68f28
sjpbailey <sjpbailey1961@gmail.com>
2022-11-15 13:37:16 -0800

    Update test_json.py

--- nodes/controllers_nodes/test_json.py
+++ nodes/controllers_nodes/test_json.py
@@ -13,22 +13,8 @@ import time
             "key": "805217605357161b",
             "devId": {'dps': {'20': True, '21': 'white', '22': 1000, '23': 1000,
                               '24': '009d03c603a1', '25': '020d0d00000000000000001903e8', '26': 0}}}}]"""
-# print(type(data))
-# data1 = {"name": "LED Office",
-# "id": "ebfc16d57ed374932cjqfk", "key": "805217605357161b"}
-# print(type(data1))
-jsondata = [{"name": "LED Office", "id": "ebfc16d57ed374932cjqfk", "key": "805217605357161b", 'dps': {'20': False, '21': 'white', '22': 10, '23': 1000,
-                                                                                                      '24': '009d03c603a1', '25': '020d0d00000000000000001903e8', '26': 0}}]
-# if jsondata['dps'] == 20:
-devices_list = json.dumps(jsondata)
-key = jsondata[1:1]
-print(key)
-# print(type(devices_list))
-# print(type(jsondata))
-# print(jsondata[0:10])
 
-# for dps in str(jsondata):  # )xfor i in node_status: gives dps devId)
-"""jsondata = {"name": "LED Office",
+jsondata = {"name": "LED Office",
             "ip": "192.168.1.147",
             "ver": "3.3",
             "id": "ebfc16d57ed374932cjqfk",
@@ -44,7 +30,20 @@ print(key)
                     "26": 0
                 }
             }
-            }"""
+            }
+
+
+# print(type(data))
+# data1 = {"name": "LED Office",
+# "id": "ebfc16d57ed374932cjqfk", "key": "805217605357161b"}
+# print(type(data1))
+# jsondata = [{"name": "LED Office", "id": "ebfc16d57ed374932cjqfk", "key": "805217605357161b", "dps": {"20": False, "21": "white", "22": 10, "23": 1000,
+#                                                                                                     "24": "009d03c603a1", "25": "020d0d00000000000000001903e8", "26": 0}}]
+devices_list = json.loads(jsondata)
+print(type(devices_list))
+for i in devices_list:
+    print(i,)
+# for dps in str(jsondata):  # )xfor i in node_status: gives dps devId)
 
 """devices_list = json.dumps(jsondata)
 print("%-25s %-24s %-16s %-17s %-5s" % ("Name", "ID", "IP", "Key", "Version"))
